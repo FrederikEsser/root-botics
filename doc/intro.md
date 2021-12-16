8cf612f06b226d1ea44d709704d9033089528acf
Frederik Esser <fe@skm.dk>
2021-12-16 20:30:03 +0100

    Empty project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to root-botics
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/root_botics/core_test.clj
@@ -0,0 +1,7 @@
+(ns root-botics.core-test
+  (:require [clojure.test :refer :all]
+            [root-botics.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
